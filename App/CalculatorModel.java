5c32aa30ec7a85cc61a3d4009822eb7fbdfb2806
ajaycbsc24-debug <ajaycbsc24@rvu.edu.in>
2025-11-09 15:00:54 +0530

    Update CalculatorModel.java

--- App/CalculatorModel.java
+++ App/CalculatorModel.java
@@ -1,5 +1,16 @@
 package app;
 
 public class CalculatorModel {
-    // TODO: Math operations logic
+    public double calculate(double a, double b, String op) {
+        return switch (op) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            case "/" -> {
+                if (b == 0) throw new ArithmeticException("Cannot divide by zero");
+                yield a / b;
+            }
+            default -> throw new IllegalArgumentException("Unknown operator");
+        };
+    }
 }
